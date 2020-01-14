9c6e9a162b85cdb99e0690e7bb075a0cdc999405
Chen <timeforbattle@hotmail.com>
2020-01-14 09:20:34 +0200

    changes to complex

--- hw4/hw4q1.py
+++ hw4/hw4q1.py
@@ -4,15 +4,15 @@ def main():
     if question == 1:
         print('The answer to question 1 is h')
     elif question == 2:
-        print('The answer to question 2 is a')
+        print('The answer to question 2 is a or k')
     elif question == 3:
-        print('The answer to question 3 is h')
+        print('The answer to question 3 is z(n^(3/2)*log(n))')
     elif question == 4:
-        print('The answer to question 4 is i')
+        print('The answer to question 4 is l')
     elif question == 5:
         print('The answer to question 5 is b')
     elif question == 6:
-        print('The answer to question 6 is v')
+        print('The answer to question 6 is z(3^(3^(n/3)))')
     else:
         print('Error')
         exit()
